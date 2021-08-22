4906eb9a8489bdbc62e3dd994c89cfe4fdb90c55
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-08-23 00:44:16 +0200

    Methods extraction

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,51 +7,69 @@ import "./page-interface-generated";
 import { EOrientation, Primitive } from "./primitives/primitive";
 
 
+type Layer = Primitive[];
+
+function computeLayers(nbLayers: number): Layer[] {
+    const basePrimitive = new Primitive(
+        { x: 0, y: 0 }, { x: 512, y: 0 }, { x: 0, y: 512 }, { x: 512, y: 512 },
+        EOrientation.VERTICAl,
+    );
+
+    const layers: Layer[] = [];
+    layers.push([basePrimitive]);
+
+    for (let i = 0; i < nbLayers; i++) {
+        let newLayer: Layer = [];
+
+        const lastlayer: Layer = layers[layers.length - 1];
+        for (const parentPrimitive of lastlayer) {
+            newLayer = newLayer.concat(parentPrimitive.subdivide());
+        }
+
+        layers.push(newLayer);
+    }
+    return layers;
+}
+
+function computeLinesBatches(layers: Layer[]): ILinesBatch[] {
+    const linesBatches: ILinesBatch[] = [];
+
+    const MAX_THICKNESS = 2;
+
+    for (let iLayer = 0; iLayer < layers.length - 1; iLayer++) {
+        const layer = layers[iLayer];
+
+        const lines: ILine[] = [];
+        for (const primitive of layer) {
+            lines.push({
+                p1: primitive.subdivision[0],
+                p2: primitive.subdivision[1],
+            });
+        }
+
+        linesBatches.push({
+            lines,
+            color: Color.BLACK,
+            thickness: 1 + MAX_THICKNESS * (layers.length - 2 - iLayer) / (layers.length - 2),
+        });
+    }
+
+    return linesBatches;
+}
+
 function main(): void {
     const plotter = new PlotterCanvas2D();
     const backgroundColor = new Color(255, 128, 128);
 
-    const basePrimitive = new Primitive({ x: 0, y: 0 }, { x: 512, y: 0 }, { x: 0, y: 512 }, { x: 512, y: 512 }, EOrientation.VERTICAl);
+    let layers: Layer[];
+    let linesBatches: ILinesBatch[];
 
-    const layers: Primitive[][] = [];
-    {
-        layers.push([basePrimitive]);
-
-        const NB_LAYERS = 10;
-        for (let i = 0; i < NB_LAYERS; i++) {
-            const lastlayer: Primitive[] = layers[layers.length - 1];
-
-            let newLayer: Primitive[] = [];
-            for (const parentPrimitive of lastlayer) {
-                newLayer = newLayer.concat(parentPrimitive.subdivide());
-            }
-
-            layers.push(newLayer);
-        }
+    function reset(): void {
+        layers = computeLayers(10);
+        linesBatches = computeLinesBatches(layers);
     }
 
-    const linesBatches: ILinesBatch[] = [];
-    {
-        const MAX_THICKNESS = 2;
-
-        for (let iLayer = 0; iLayer < layers.length - 1; iLayer++) {
-            const layer = layers[iLayer];
-
-            const lines: ILine[] = [];
-            for (const primitive of layer) {
-                lines.push({
-                    p1: primitive.subdivision[0],
-                    p2: primitive.subdivision[1],
-                });
-            }
-
-            linesBatches.push({
-                lines,
-                color: Color.BLACK,
-                thickness: 1 + MAX_THICKNESS * (layers.length - 2 - iLayer) / (layers.length - 2),
-            });
-        }
-    }
+    reset();
 
     function mainLoop(): void {
         plotter.initialize(backgroundColor);
