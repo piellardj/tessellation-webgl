1be1317409c42ded3ef30a69f53db123c0a56bbe
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-10-03 15:49:04 +0200

    Don't draw if nothing to draw

--- src/ts/plotter/plotter-canvas-2d.ts
+++ src/ts/plotter/plotter-canvas-2d.ts
@@ -17,43 +17,49 @@ class PlotterCanvas2D extends PlotterBase {
         this.context.fillStyle = "none";
         this.context.strokeStyle = (alpha >= 1) ? color.toHexaString() : color.toRgbaString(alpha);
     public drawLines(linesBatches: ILinesBatch[], color: Color, alpha: number): void {
+        if (alpha > 0 && linesBatches) {
+            this.context.fillStyle = "none";
+            this.context.strokeStyle = (alpha >= 1) ? color.toHexaString() : color.toRgbaString(alpha);
 
-        const halfWidth = 0.5 * this.width;
-        const halfHeight = 0.5 * this.height;
-        for (const linesBatch of linesBatches) {
-            this.context.lineWidth = linesBatch.thickness;
+            const halfWidth = 0.5 * this.width;
+            const halfHeight = 0.5 * this.height;
+            for (const linesBatch of linesBatches) {
+                this.context.lineWidth = linesBatch.thickness;
 
-            this.context.beginPath();
-            for (const line of linesBatch.lines) {
-                if (line.length >= 2) {
-                    this.context.moveTo(line[0].x + halfWidth, line[0].y + halfHeight);
+                this.context.beginPath();
+                for (const line of linesBatch.lines) {
+                    if (line.length >= 2) {
+                        this.context.moveTo(line[0].x + halfWidth, line[0].y + halfHeight);
 
-                    for (let iP = 1; iP < line.length; iP++) {
-                        this.context.lineTo(line[iP].x + halfWidth, line[iP].y + halfHeight);
+                        for (let iP = 1; iP < line.length; iP++) {
+                            this.context.lineTo(line[iP].x + halfWidth, line[iP].y + halfHeight);
+                        }
                     }
                 }
+                this.context.stroke();
+                this.context.closePath();
             }
-            this.context.stroke();
-            this.context.closePath();
         }
     }
 
-        this.context.strokeStyle = "none";
     public drawPolygons(polygons: IPolygon[], alpha: number): void {
+        if (alpha > 0 && polygons) {
+            this.context.strokeStyle = "none";
 
-        const halfWidth = 0.5 * this.width;
-        const halfHeight = 0.5 * this.height;
-        for (const polygon of polygons) {
-            if (polygon.points.length >= 3) {
-                this.context.fillStyle = (alpha >= 1) ? polygon.color.toHexaString() : polygon.color.toRgbaString(alpha);
+            const halfWidth = 0.5 * this.width;
+            const halfHeight = 0.5 * this.height;
+            for (const polygon of polygons) {
+                if (polygon.points.length >= 3) {
+                    this.context.fillStyle = (alpha >= 1) ? polygon.color.toHexaString() : polygon.color.toRgbaString(alpha);
 
-                this.context.beginPath();
-                this.context.moveTo(polygon.points[0].x + halfWidth, polygon.points[0].y + halfHeight);
-                for (let iP = 1; iP < polygon.points.length; iP++) {
-                    this.context.lineTo(polygon.points[iP].x + halfWidth, polygon.points[iP].y + halfHeight);
+                    this.context.beginPath();
+                    this.context.moveTo(polygon.points[0].x + halfWidth, polygon.points[0].y + halfHeight);
+                    for (let iP = 1; iP < polygon.points.length; iP++) {
+                        this.context.lineTo(polygon.points[iP].x + halfWidth, polygon.points[iP].y + halfHeight);
+                    }
+                    this.context.closePath();
+                    this.context.fill();
                 }
-                this.context.closePath();
-                this.context.fill();
             }
         }
     }
--- src/ts/plotter/plotter-webgl.ts
+++ src/ts/plotter/plotter-webgl.ts
@@ -56,7 +56,7 @@ class PlotterWebGL extends PlotterBase {
     }
 
     public drawLines(linesBatches: ILinesBatch[], color: Color, alpha: number): void {
-        if (this.shaderLines && alpha > 0) {
+        if (this.shaderLines && alpha > 0 && linesBatches) {
             const vertexData: number[] = [];
             for (const linesBatch of linesBatches) {
                 for (const line of linesBatch.lines) {
@@ -77,19 +77,21 @@ class PlotterWebGL extends PlotterBase {
                 }
             }
 
-            const data = new Float32Array(vertexData);
-            this.linesVBO.setData(data);
-            this.shaderLines.u["uColor"].value = [color.r / 255, color.g / 255, color.b / 255, alpha];
-            this.shaderLines.u["uScreenSize"].value = [0.5 * this.width, -0.5 * this.height];
+            if (vertexData.length > 0) {
+                const data = new Float32Array(vertexData);
+                this.linesVBO.setData(data);
+                this.shaderLines.u["uColor"].value = [color.r / 255, color.g / 255, color.b / 255, alpha];
+                this.shaderLines.u["uScreenSize"].value = [0.5 * this.width, -0.5 * this.height];
 
-            this.shaderLines.use();
-            this.shaderLines.bindUniformsAndAttributes();
-            gl.drawArrays(gl.LINES, 0, data.length / 2);
+                this.shaderLines.use();
+                this.shaderLines.bindUniformsAndAttributes();
+                gl.drawArrays(gl.LINES, 0, data.length / 2);
+            }
         }
     }
 
     public drawPolygons(polygons: IPolygon[], alpha: number): void {
-        if (this.shaderPolygons && alpha > 0) {
+        if (this.shaderPolygons && alpha > 0 && polygons) {
             const vertexData: number[] = [];
             for (const polygon of polygons) {
                 if (polygon.points.length >= 3) {
@@ -122,23 +124,25 @@ class PlotterWebGL extends PlotterBase {
                 }
             }
 
-            this.shaderPolygons.u["uScreenSize"].value = [0.5 * this.width, -0.5 * this.height];
+            if (vertexData.length > 0) {
+                this.shaderPolygons.u["uScreenSize"].value = [0.5 * this.width, -0.5 * this.height];
 
-            this.shaderPolygons.use();
-            this.shaderPolygons.bindUniforms();
+                this.shaderPolygons.use();
+                this.shaderPolygons.bindUniforms();
 
-            const data = new Float32Array(vertexData);
-            const BYTES_PER_FLOAT = 4;
-            const aPositionLoc = this.shaderPolygons.a["aPosition"].loc;
-            const aColorLoc = this.shaderPolygons.a["aColor"].loc;
-            gl.bindBuffer(gl.ARRAY_BUFFER, this.polygonsVBOId);
-            gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW);
-            gl.enableVertexAttribArray(aPositionLoc);
-            gl.vertexAttribPointer(aPositionLoc, 2, gl.FLOAT, false, BYTES_PER_FLOAT * 6, 0);
-            gl.enableVertexAttribArray(aColorLoc);
-            gl.vertexAttribPointer(aColorLoc, 4, gl.FLOAT, false, BYTES_PER_FLOAT * 6, BYTES_PER_FLOAT * 2);
+                const data = new Float32Array(vertexData);
+                const BYTES_PER_FLOAT = 4;
+                const aPositionLoc = this.shaderPolygons.a["aPosition"].loc;
+                const aColorLoc = this.shaderPolygons.a["aColor"].loc;
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.polygonsVBOId);
+                gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW);
+                gl.enableVertexAttribArray(aPositionLoc);
+                gl.vertexAttribPointer(aPositionLoc, 2, gl.FLOAT, false, BYTES_PER_FLOAT * 6, 0);
+                gl.enableVertexAttribArray(aColorLoc);
+                gl.vertexAttribPointer(aColorLoc, 4, gl.FLOAT, false, BYTES_PER_FLOAT * 6, BYTES_PER_FLOAT * 2);
 
-            gl.drawArrays(gl.TRIANGLES, 0, data.length / 6);
+                gl.drawArrays(gl.TRIANGLES, 0, data.length / 6);
+            }
         }
     }
 
