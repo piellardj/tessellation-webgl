5029a88b3408b50c6e84f77324a7e057b9037ca8
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-10-11 19:17:40 +0200

    Linting

--- src/ts/engine/engine-interface.ts
+++ src/ts/engine/engine-interface.ts
@@ -6,8 +6,8 @@ import { EPrimitiveType } from "../primitives/primitive-type-enum";
 
 interface IEngine {
     update(viewport: Rectangle, instantZoom: Zoom, wantedDepth: number, subdivisionBalance: number, colorVariation: number): boolean;
-    draw(plotter: PlotterBase, scaling: number): void
-    reset(viewport: Rectangle, primitiveType: EPrimitiveType): void
+    draw(plotter: PlotterBase, scaling: number): void;
+    reset(viewport: Rectangle, primitiveType: EPrimitiveType): void;
     recomputeColors(colorVariation: number): void;
 }
 
